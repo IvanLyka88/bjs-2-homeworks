60f178f5e1c19ced031c01dc2bb76b6662a38934
Ivan Lyka <luk_luka@bk.ru>
2023-05-20 22:16:45 +0300

    Первый вариант

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿"use strict"
+
+function parseCount(num) {
+    const parsed = Number.parseInt(num);
+  if (isNaN(parsed)) {
+    throw new Error('Невалидное значение');
+  }
+  return parsed;
+}
+
+function validateCount(num) {
+  try {
+return parseCount(num);
+
+  }
+  catch (error){
+   return error;
+  }
+}
+
+
+class Triangle {
+
+    constructor(a, b, c) {
+  
+      this.a = a;
+      this.b = b;
+      this.c = c;
+  
+      if (this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
+        throw new Error('Треугольник с такими сторонами не существует')
+      }
+    }
+  
+    getPerimeter() {
+  
+      return this.a + this.b + this.c;
+    }
+  
+    getArea() {
+      const p = this.getPerimeter() / 2;
+      return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+  
+    try {
+      return new Triangle(a, b, c);
+    }
+    catch {
+  
+      return {
+        getArea: function () { return 'Ошибка! Треугольник не существует' },
+        getPerimeter: function () { return 'Ошибка! Треугольник не существует' }
+      }
+  
+    }
+  }
