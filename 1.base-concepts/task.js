e571100890874a3ecff81fad43c5084108043b35
Ivan Lyka <luk_luka@bk.ru>
2023-05-22 23:55:47 +0300

    Ready-made version

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let D = Math.pow(b, 2) - 4 * a * c;
+  if (D < 0) {
+    return arr;
+  } else if (D === 0) {
+    let X = -b / (2 * a);
+    return (arr = [X]);
+  } else if (D > 0) {
+    let X1 = (-b + Math.sqrt(D)) / (2 * a);
+    let X2 = (-b - Math.sqrt(D)) / (2 * a);
+    return (arr = [X1, X2]);
+  }
 }
