584cd79618ddba6f1c8968f38a6386d10409cf78
Ivan Lyka <luk_luka@bk.ru>
2023-05-29 23:47:50 +0300

    Ready-made version 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
-function compareArrays(arr1, arr2) {
-  
-}
+const compareArrays = (arr1, arr2) =>
+  arr1.length === arr2.length &&
+  arr1.every((element, index) => element === arr2[index]);
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    return users
+      .filter((person) => person.gender == gender)
+      .map((person) => person.age)
+      .reduce((age, person, index, arrOfAge) => {
+        return age + person / arrOfAge.length;
+      }, 0);
+  }
